3d36177987732cd45f14981600150967463ba673
CinareT <cinare199818@gmail.com>
2024-05-17 11:54:11 +0400

    setted dinamic and static data translatable

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,8 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Translatable\HasTranslations;
 
 class Category extends Model
 {
-    use HasFactory;
+    use HasFactory, HasTranslations;
+
+    protected $fillable = [
+        'title',
+        'slug',
+    ];
+
+    protected $translatable = ['title','slug'];
+
 }
